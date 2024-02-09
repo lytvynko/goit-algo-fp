1fcd03e65c4f5d46332855fa1842abcc190dd5c3
Dmytro Lytvynko <agrrbagrr1986@gmail.com>
2024-02-09 15:40:59 +0200

    updated 2.py and created README.MD

--- 2.py
+++ 2.py
@@ -1,16 +1,16 @@
 import turtle
 
-def draw_tree(branch_len, t, recursion_depth):
+def draw_tree(branch_len, t, recursion_depth, shrink_factor):
     if recursion_depth > 0:
         t.forward(branch_len)
         t.right(25)
-        draw_tree(branch_len - 15, t, recursion_depth - 1)
+        draw_tree(branch_len * shrink_factor, t, recursion_depth - 1, shrink_factor)
         t.left(50)
-        draw_tree(branch_len - 15, t, recursion_depth - 1)
+        draw_tree(branch_len * shrink_factor, t, recursion_depth - 1, shrink_factor)
         t.right(25)
         t.backward(branch_len)
 
-def main(recursion_depth=5):
+def main(recursion_depth=5, shrink_factor=0.7):
     t = turtle.Turtle()
     my_win = turtle.Screen()
     t.left(90)
@@ -18,7 +18,7 @@ def main(recursion_depth=5):
     t.backward(100)
     t.down()
     t.color("green")
-    draw_tree(100, t, recursion_depth)
+    draw_tree(100, t, recursion_depth, shrink_factor)
     my_win.exitonclick()
 
-main(7)
+main(7, 0.7)
